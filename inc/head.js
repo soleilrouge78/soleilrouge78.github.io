0a8094d02dc87507178e985d7257065aeaa687f5
Martino Zanetti <martino.zanetti@gmail.com>
2026-05-14 12:06:24 +0200

    first commit

---
+++ inc/head.js
@@ -0,0 +1,8 @@
+document.write('<head>');
+document.write('    <title>Soleil Rouge</title>');
+document.write('    <link rel="shortcut icon" href="img/logotrasp.png">');
+document.write('    <meta charSet="UTF-8">');
+document.write('    <meta name="viewport" content="width=device-width, initial-scale=1">');
+document.write('    <link rel="stylesheet" href="css/w3style.css">');
+document.write('</head>');
+
